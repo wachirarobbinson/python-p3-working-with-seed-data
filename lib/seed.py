f01c9b2e5e1beb938fb8df4a377bcb8794872065
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-09-07 06:01:33 +0300

    done

--- lib/seed.py
+++ lib/seed.py
@@ -10,7 +10,22 @@ from models import Game
 fake = Faker()
 
 if __name__ == '__main__':
-    
+
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    print("Seeding games...")
+
+    games = [
+        Game(
+            title=fake.name(),
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(0, 60)
+        )
+        for i in range(50)]
+
+    session.bulk_save_objects(games)
+    session.commit()
+    session.query(Game).count()
